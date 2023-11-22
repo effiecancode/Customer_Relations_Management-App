97647a4906d087832854efbf8c6058ee47f0602f
effiecancode <faitheffie25@gmail.com>
2023-11-22 14:08:06 +0300

    Account urls mapping

--- dcrm/Account/views.py
+++ dcrm/Account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
--- dcrm/dcrm/urls.py
+++ dcrm/dcrm/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    
+
+    path('Account', include('Account.urls')),
+
 ]
